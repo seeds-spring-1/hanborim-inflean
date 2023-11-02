b0f734c113ad3a2836669d58b8823198689b9206
hanborim03 <a01082469291@gmail.com>
2023-11-02 23:42:21 +0900

    섹션6: 스프링 JdbcTemplate

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,35 +1,26 @@
 package hello.hellospring;
-
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
-
-    @Autowired
+    private final DataSource dataSource;
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
-    public MemberRepository memberRepository(){
-        //return  new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+// return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
